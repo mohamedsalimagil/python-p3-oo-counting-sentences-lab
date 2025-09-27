bcaead2c015250966bc044313ba41eedad959c1e
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-09-27 16:53:06 +0300

    python-p3-oo-counting-sentences-lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value  # runs through the setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+            
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+      
+    def is_question(self):
+        return self.value.endswith("?")
+    
+    def is_exclamation(self):
+        return self.value.endswith("!")
+      
+    def count_sentences(self):
+        count = 0
+        for i in range(len(self.value)):
+            if self.value[i] in ".?!" and (i == len (self.value) -1 or self.value[i+1] not in ".?!"):
+               count += 1
+        return count
+        
